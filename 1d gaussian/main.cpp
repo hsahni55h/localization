3fa9c84f480f9a2c603c651f4a6bf8bd49a4c3aa
himanshu <h.sahni1998@gmail.com>
2023-08-30 12:32:02 +0200

    commit

--- 1d gaussian/main.cpp
+++ 1d gaussian/main.cpp
@@ -7,7 +7,7 @@ double f(double mu, double sigma2, double x)
 {
     //Use mu, sigma2 (sigma squared), and x to code the 1-dimensional Gaussian
     //Put your code here
-    double prob = 1 / sqrt(2 * sigma2 * M_PI ) * exp( - 0.5 * pow((x-mu) , 2) / sigma2);
+    double prob = 1.0 / sqrt(2.0 * M_PI * sigma2) * exp(-0.5 * pow((x - mu), 2.0) / sigma2);
     return prob;
 }
 
