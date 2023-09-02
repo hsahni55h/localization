283649d19d2d891a32ca723814893e813c7da1a9
himanshu <h.sahni1998@gmail.com>
2023-09-02 11:31:46 +0200

    commit

--- multidimensional_kalman_filter/main.cpp
+++ multidimensional_kalman_filter/main.cpp
@@ -1,78 +1,21 @@
-#include <iostream>
-#include <math.h>
-#include <tuple>
-#include "Core" // Eigen Library
-#include "LU"   // Eigen Library
+# References
 
-using namespace std;
-using namespace Eigen;
+# Compiler
+CC = g++
 
-// float measurements[3] = { 1, 2, 3 };
+# Compiler flags
+CFLAGS = -std=c++11 -Wall -g3
 
-// tuple<MatrixXf, MatrixXf> kalman_filter(MatrixXf x, MatrixXf P, MatrixXf u, MatrixXf F, MatrixXf H, MatrixXf R, MatrixXf I)
-// {
-//     for (int n = 0; n < sizeof(measurements) / sizeof(measurements[0]); n++) {
-//         //****** TODO: Kalman-filter function********//
-        
-//         // Measurement Update
-//         // Code the Measurement Update
-//         // Initialize and Compute Z, y, S, K, x, and P
-        
-//         // Z = [1 0]
-//         // y = z - H * x'
+# Source file and target executable
+SRC = main.cpp
+BUILD_DIR = build
+TARGET = $(BUILD_DIR)/main.out
 
-//         // S = H*P'H.T + R
+all: $(TARGET)
 
+$(TARGET): $(SRC)
+	mkdir -p $(BUILD_DIR)
+	$(CC) $(CFLAGS) $(SRC) -o $(TARGET)
 
-        
-        
-        
-        
-        
-        
-//         // Prediction
-//         // Code the Prediction
-//         // Compute x and P
-
-//         // x = x' + Ky
-//         // P = (I - KH)P'
-
-
-
-        
-        
-//     }
-
-//     return make_tuple(x, P);
-// }
-
-int main()
-{
-
-    // MatrixXf x(2, 1);// Initial state (location and velocity) 
-    // x << 0,
-    // 	 0; 
-    // MatrixXf P(2, 2);//Initial Uncertainty
-    // P << 100, 0, 
-    // 	 0, 100; 
-    // MatrixXf u(2, 1);// External Motion
-    // u << 0,
-    // 	 0; 
-    // MatrixXf F(2, 2);//Next State Function
-    // F << 1, 1,
-    // 	 0, 1; 
-    // MatrixXf H(1, 2);//Measurement Function
-    // H << 1,
-    // 	 0; 
-    // MatrixXf R(1, 1); //Measurement Uncertainty
-    // R << 1;
-    // MatrixXf I(2, 2);// Identity Matrix
-    // I << 1, 0,
-    // 	 0, 1; 
-
-    // tie(x, P) = kalman_filter(x, P, u, F, H, R, I);
-    cout << "x= " << x << endl;
-    cout << "P= " << P << endl;
-
-    return 0;
-}
+clean:
+	rm -rf $(BUILD_DIR)
