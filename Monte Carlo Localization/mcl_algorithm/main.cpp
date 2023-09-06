a9e3c50b319dc383121245730370b903054ad36a
himanshu <h.sahni1998@gmail.com>
2023-09-06 14:03:48 +0200

    commit

--- Monte Carlo Localization/mcl_algorithm/main.cpp
+++ Monte Carlo Localization/mcl_algorithm/main.cpp
@@ -227,25 +227,30 @@ void visualization(int n, Robot robot, int step, Robot p[], Robot pr[])
 //####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 int main()
 {
-    // Instantiating a robot object from the Robot class
+    // TODO: Instantiate a robot object from the Robot class
     Robot myrobot;
 
-    // TODO: Set robot new position to x=10.0, y=10.0 and orientation=0
-    // Fill in the position and orientation values in myrobot.set() function
-    myrobot.set(10.0, 10.0, 0);
-
-    // Printing out the new robot position and orientation
+    // TODO: Set robot new position to x=30.0, y=50.0 and orientation=PI/2
+    myrobot.set(30.0, 50.0, M_PI/2);
+    cout << "First command"<< endl;
     cout << myrobot.show_pose() << endl;
 
-    // TODO: Rotate the robot by PI/2.0 and then move him forward by 10.0
-    // Use M_PI for the pi value
-    myrobot.move(M_PI / 2, 10.0);
-
-    // TODO: Print out the new robot position and orientation
+    // TODO: Turn clockwise by PI/2 and move by 15 meters
+    myrobot.move(M_PI / 2, 15.0);
+    cout << "Second command"<< endl;
     cout << myrobot.show_pose() << endl;
 
-    // Printing the distance from the robot toward the eight landmarks
+    // TODO: Print the distance from the robot toward the eight landmarks
+    cout << "Distane from sensors"<< endl;
     cout << myrobot.read_sensors() << endl;
 
+    // TODO: Turn clockwise by PI/2 and move by 10 meters
+    myrobot.move(M_PI / 2, 10.0);
+    cout << "Third command"<< endl;
+    cout << myrobot.show_pose() << endl;
+
+    // TODO: Print the distance from the robot toward the eight landmarks
+    cout << "Distane from sensors"<< endl;
+    cout << myrobot.read_sensors() << endl;
     return 0;
 }
