3e6999d43d884e7d7182e349c0bd74d26e642917
himanshu <h.sahni1998@gmail.com>
2023-09-06 14:06:28 +0200

    commit

--- Monte Carlo Localization/mcl_algorithm/main.cpp
+++ Monte Carlo Localization/mcl_algorithm/main.cpp
@@ -113,7 +113,7 @@ public:
         // Returns all the distances from the robot toward the landmarks
         vector<double> z = sense();
         string readings = "[";
-        for (int i = 0; (long unsigned)i < z.size(); i++) {
+        for (int i = 0; (long unsigned) i < z.size(); i++) {
             readings += to_string(z[i]) + " ";
         }
         readings[readings.size() - 1] = ']';
@@ -227,30 +227,16 @@ void visualization(int n, Robot robot, int step, Robot p[], Robot pr[])
 //####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 int main()
 {
-    // TODO: Instantiate a robot object from the Robot class
     Robot myrobot;
-
-    // TODO: Set robot new position to x=30.0, y=50.0 and orientation=PI/2
-    myrobot.set(30.0, 50.0, M_PI/2);
-    cout << "First command"<< endl;
-    cout << myrobot.show_pose() << endl;
-
-    // TODO: Turn clockwise by PI/2 and move by 15 meters
-    myrobot.move(M_PI / 2, 15.0);
-    cout << "Second command"<< endl;
-    cout << myrobot.show_pose() << endl;
-
-    // TODO: Print the distance from the robot toward the eight landmarks
-    cout << "Distane from sensors"<< endl;
+    // TODO: Simulate Noise
+    // Forward Noise=5.0, Turn Noise=0.1,Sense Noise=5.0
+    myrobot.set_noise(5.0, 0.1, 5.0);
+    
+    myrobot.set(30.0, 50.0, M_PI / 2.0);
+    myrobot.move(-M_PI / 2.0, 15.0);
+    cout << myrobot.read_sensors() << endl;
+    myrobot.move(-M_PI / 2.0, 10.0);
     cout << myrobot.read_sensors() << endl;
 
-    // TODO: Turn clockwise by PI/2 and move by 10 meters
-    myrobot.move(M_PI / 2, 10.0);
-    cout << "Third command"<< endl;
-    cout << myrobot.show_pose() << endl;
-
-    // TODO: Print the distance from the robot toward the eight landmarks
-    cout << "Distane from sensors"<< endl;
-    cout << myrobot.read_sensors() << endl;
     return 0;
 }
